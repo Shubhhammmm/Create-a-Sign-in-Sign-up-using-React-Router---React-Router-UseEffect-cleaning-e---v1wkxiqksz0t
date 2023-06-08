1f221b06c61df1927a8b720a0a3d6843cd1c43ed
Shubham Shrivastava <s8982034020@gmail.com>
2023-06-08 11:44:22 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,66 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-import { BrowserRouter as Router ,Route,Switch ,Redirect,NavLink,Link} from 'react-router-dom' ;
-import Login from './Login.js'
-import Register from './Register.js'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
 import Dashboard from './Dashboard';
 
-
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [navigate, setNavigate] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-//code for  Route path="/" , Route path="/dashboard" 
+  const handleLogin = () => {
+    if (email === email && password === password) {
+      setLoggedIn(true);
+      setNavigate(true);
+    }
+  };
 
-  const [loggedIn,setLoggedIn]=useState(false)
- const [navigate,setNavigate]=useState(false)
-  const[email,setEmail]=useState('')
-  const[password,setPassword]=useState('')
   return (
+    <div id="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {loggedIn ? (
+              <Redirect to="/dashboard" />
+            ) : (
+              <Login
+                email={email}
+                password={password}
+                setLoggedIn={setLoggedIn}
+                setNavigate={setNavigate}
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleLogin={handleLogin}
+              />
+            )}
+          </Route>
 
-<div id='App'>
-  <Router>
-    
-    <Switch>
-      <Route path="/" exact>
-     <Login email={email} password={password} setLoggedIn={setLoggedIn} setNavigate={setNavigate}/>
-      </Route>
-      
-      <Route path="/Register" exact>
-        {navigate? <Redirect to="/" /> : <Register  emailChange={setEmail}  passwordChange={setPassword} setNavigate={setNavigate} />}
-      </Route>
+          <Route path="/Register" exact>
+            {navigate ? (
+              <Redirect to="/" />
+            ) : (
+              <Register
+                emailChange={setEmail}
+                passwordChange={setPassword}
+                setNavigate={setNavigate}
+              />
+            )}
+          </Route>
 
-      <Route path="/dashboard" exact>
-       <Dashboard setLoggedIn={setLoggedIn}/> 
-      </Route>
-    </Switch>
-  </Router>
-</div>
-  )
-}
-
+          <Route path="/dashboard" exact>
+            {loggedIn ? (
+              <Dashboard setLoggedIn={setLoggedIn} />
+            ) : (
+              <Redirect to="/" />
+            )}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,18 @@
-import React from "react";
-
+import React from 'react';
 
 function Dashboard({ setLoggedIn }) {
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
 
-    return (
-        <div id="dashboardPage">
-            <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
-            <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
-        </div>
-    )
-
+  return (
+    <div id="dashboardPage">
+      <h1 id="dashboard-tag">Welcome to Dashboard</h1>
+      <button id="dashboard-logout" onClick={handleLogout}>
+        Log out
+      </button>
+    </div>
+  );
 }
+
 export default Dashboard;
